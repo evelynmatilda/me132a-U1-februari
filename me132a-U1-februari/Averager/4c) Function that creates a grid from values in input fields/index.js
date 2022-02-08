63e579525248cdc314430075ba39e0399b42f9e9
evelynmatilda <evelyn.palsson@gmail.com>
2022-02-08 18:06:16 +0100

    Me132a evelyn

---
+++ me132a-U1-februari/Averager/4c) Function that creates a grid from values in input fields/index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+function gridMaker (gridContainer, R, C) {
+    gridContainer.style.display = `grid`;
+    gridContainer.style.height = `50vh`;
+    gridContainer.style.height = `50vh`;
+    gridContainer.style.gridTemplateRows = `repeat(${R}, 1fr)` ;
+    gridContainer.style.gridTemplateColumns = `repeat(${C}, 1fr)` ;
+}
+
+document.querySelector("button").addEventListener( "click", function () {
+    gridMaker( document.querySelector("#grid"), document.querySelector("#inputRows").value, document.querySelector("#inputCols").value )
+});
+
+/*
+
+We will continue with our work from 4b.
+We need to fix these two things:
+
+First:
+gridMaker must be called each time the user clicks on the button (See index.html).
+
+
+Second:
+We now need to make sure that when we call (anropar) gridMaker we must use
+the values in #inputRows and #inputColumns as arguments.
+
+*/
+
---
+++ me132a-U1-februari/Averager/6) CSS formatting/index.js
@@ -0,0 +1,45 @@
+"use strict";
+
+
+/*
+
+Use a CSS-file to make the grid, the input fields, the results and the button look nice.
+You're free to format this as you want, but I expect some effort. See my intial video
+to get a sense of the level of formatting expected.
+
+*/
+function createNumberDiv () {
+   let numberDiv = document.createElement ("div");
+   numberDiv.innerHTML = Math.floor( 99 * Math.random ());
+   numberDiv.addEventListener ("click", function () {
+   numberDiv.classList.toggle ("selected");
+  
+numberDiv.addEventListener ("click", updateResults ("selected"))
+});
+  return numberDiv;
+};
+
+
+function gridMaker (gridContainer, R, C) {
+    gridContainer.style.gridTemplateRows = `repeat(${R}, 1fr)` ;
+    gridContainer.style.gridTemplateColumns = `repeat(${C}, 1fr)` ;
+    for (let c = 0; c < C; c++) {
+        for (let r = 0; r < R; r++) {
+        gridContainer.appendChild( createNumberDiv() );
+        }
+    }
+}
+
+document.querySelector("button").addEventListener("click", function () {
+
+    gridMaker(
+      document.querySelector("#grid"),
+      document.querySelector("#inputRows").value,
+      document.querySelector("#inputCols").value
+   );
+  
+});
+
+function randomNumber (max) {
+    return Math.floor(max * Math.random());
+}
